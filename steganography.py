8b6360b9777a8b2c90e0f07d242b05f0ddbb8956
yyoussefeldeeb <ye2300964@tkh.edu.eg>
2024-12-03 00:42:21 +0200

    Update steganography.py

--- steganography.py
+++ steganography.py
@@ -7,11 +7,9 @@ def hide_message_in_bmp(input_bmp, output_bmp, secret_message):
         # this line changes the secret message to binary and adds 8 zero bits to know when it ended 
         message_bits = ''.join(f'{ord(char):08b}' for char in secret_message) + '00000000'
 
-    
         # a checker to see if the message will fit in the file 
         if len(message_bits) > (len(data) - 54) * 8:  # Header is 54 bytes
             raise ValueError(" image too small to write the secret message.")
-
 
         # putting the binary message in the image 
         bit_index = 0
@@ -40,12 +38,12 @@ def extract_message_from_bmp(input_bmp):
         message_bits.append(byte & 1)  # Get the least significant bit
     return message_bits
 
-    # group bits into bytes then convert to characters 
+
 def binary_to_str(message_bits):
     message_chars = []
-    for i in range(0, len(message_bits), 8):   
+    for i in range(0, len(message_bits), 8):
         byte = message_bits[i:i + 8]
-        char = chr(int(''.join(map(str,byte)), 2))
+        char = chr(int(''.join(map(str, byte)), 2))
         if char == '\x00':  # stop at the null terminator 
             break
         message_chars.append(char)
@@ -56,7 +54,7 @@ def binary_to_str(message_bits):
 # program
 print("choose an option")
 print("1. write a secret message using an BMP file")
-print("2. revel a secret message from an BMP file")
+print("2. reveal a secret message from an BMP file")
 mode = input("choose 1 or 2: ")
 
 if mode == "1":
@@ -70,8 +68,7 @@ elif mode == "2":
     # extract message 
     input_bmp = input("enter path of the output of BMP file ")
     extracted_message = extract_message_from_bmp(input_bmp)
-    string_message=binary_to_str(extracted_message)
-    print(string_message)
+    string_message = binary_to_str(extracted_message)
     print("secret message:", string_message)
 
 else:
